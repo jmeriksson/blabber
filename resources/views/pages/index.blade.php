e1077bd4943aab596098dfbef0a308c2ea4c0fc1
Mikael Eriksson <mikael.eriksson.nkpg@gmail.com>
2020-01-07 18:22:32 +0100

    Adding layouts with Blade.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'Blabber')}}</title>
-    </head>
-    <body>
-        <h1>Welcome</h1>
-        <p>This is Blabber</p>
-    </body>
-</html>
+@section('content')
+  <h1>Welcome</h1>
+  <p>This is Blabber</p>
+@endsection
